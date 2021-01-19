63efad327e9a11b3fddd37ac3f09b870bfbc9c22
Branislav Nebus <branislav.nebus@kosickaakademia.sk>
2021-01-19 11:16:10 +0100

    added a new branch id

--- src/sk/kosickaakademia/regex/Main.java
+++ src/sk/kosickaakademia/regex/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         NoRegex noRegex = new NoRegex();
         System.out.println(noRegex.isValidSPZ("KK2 7KK"));
+
+        System.out.println(noRegex.isValidID("HU159876"));
     }
 }
