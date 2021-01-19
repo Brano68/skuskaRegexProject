1a351638b07d218b56b34beb7e0dc1fe0c632c59
Branislav Nebus <branislav.nebus@kosickaakademia.sk>
2021-01-19 11:10:01 +0100

    creted a new branch regex

--- src/sk/kosickaakademia/regex/Main.java
+++ src/sk/kosickaakademia/regex/Main.java
@@ -2,6 +2,7 @@ package sk.kosickaakademia.regex;
 
 public class Main {
     public static void main(String[] args) {
-
+        NoRegex noRegex = new NoRegex();
+        System.out.println(noRegex.isValidSPZ("KK2 7KK"));
     }
 }
