0f2f3400c370fdbc4bda6fdbaf000137288043f5
Branislav Nebus <branislav.nebus@kosickaakademia.sk>
2021-01-25 17:29:28 +0100

    added an array

--- src/sk/kosickaakademia/regex/json/Jsonik.java
+++ src/sk/kosickaakademia/regex/json/Jsonik.java
@@ -8,6 +8,10 @@ public class Jsonik {
     public static void urobJson(){
         JSONArray sports = new JSONArray();
 
+        sports.add("Hockey");
+        sports.add("Skates");
+        sports.add("Swimming");
+
         JSONObject obj = new JSONObject();
 
         obj.put("name", "Milan");
@@ -15,7 +19,14 @@ public class Jsonik {
         obj.put("sport", sports);
         obj.put("active", true);
 
+
         System.out.print(obj);
+        System.out.println();
+        //vypis listu ale iba sportu
+        System.out.println("----");
+        for(int i = 0; i < sports.size(); i++){
+            System.out.println(sports.get(i));
+        }
     }
 
 
